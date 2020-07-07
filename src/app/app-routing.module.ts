6ac96503fe036db3c3196d58894d15db9b69041e
anana <anana@mellanox.com>
2020-07-07 17:18:22 +0300

    basic-project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UfmRoutes} from "./ufm/ufm.routes";
 
 
-const routes: Routes = [];
+const routes:Routes = [
+  ...UfmRoutes,
+  {
+    path: '**',
+    redirectTo: 'ufm'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
